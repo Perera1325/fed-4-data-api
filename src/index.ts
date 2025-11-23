ab3b2d7a994ecf513f20775d0a3b783f60d558df
ManupaDev <manupadevsuite@gmail.com>
2025-11-23 23:23:34 +0530

    feat: add cron job for automated energy generation record creation and update dependencies

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import energyGenerationRecordRouter from "./api/energy-generation-record";
 import { globalErrorHandler } from "./api/middlewares/global-error-handling-middleware";
 import { loggerMiddleware } from "./api/middlewares/logger-middleware";
 import { connectDB } from "./infrastructure/db";
+import { initializeEnergyCron } from "./infrastructure/energy-generation-cron";
 
 const server = express();
 server.use(cors({ origin: "http://localhost:5173" }));
@@ -18,6 +19,7 @@ server.use("/api/energy-generation-records", energyGenerationRecordRouter);
 server.use(globalErrorHandler);
 
 connectDB();
+initializeEnergyCron();
 
 const PORT = process.env.PORT || 8001;
 server.listen(PORT, () => {
